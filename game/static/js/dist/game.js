5300bf88c0b7e485954eac7995917ac259179227
iaekyx <2776870189@qq.com>
2023-07-16 22:22:59 +0800

    fix menu and playground bug

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -17,9 +17,9 @@ class AcGameMenu{
 </div>
 `);
         this.root.$ac_game.append(this.$menu);
-        this.$single_mode = this.$menu.find('ac-game-menu-field-item-single-mode');
-        this.$multi_mode = this.$menu.find('ac-game-menu-field-item-multi-mode');
-        this.$settings = this.$menu.find('ac-game-menu-field-item-settings');
+        this.$single_mode = this.$menu.find('.ac-game-menu-field-item-single-mode');
+        this.$multi_mode = this.$menu.find('.ac-game-menu-field-item-multi-mode');
+        this.$settings = this.$menu.find('.ac-game-menu-field-item-settings');
         this.start();
     }
     start(){
